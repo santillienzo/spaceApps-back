93cbca5ea38962070de11e3df04b78eaf6c3abe1
Enzo Santilli <enzo135246@gmail.com>
2021-10-16 00:32:14 -0300

    Solución

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>console.log('Database conected'))
+console.log(process.env.DATABASE)
 
 //ROUTES
 app.use('/email', require('./routes/email'));
